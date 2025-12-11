61e170d365c8d62395e7522718f8636a9062ff0e
hanna.haataja <hanna.haataja@solita.fi>
2025-12-11 16:42:11 +0200

    Add day 11 part 1

--- src/main/kotlin/aoc25/day11.kt
+++ src/main/kotlin/aoc25/day11.kt
@@ -4,8 +4,8 @@ import java.io.File
 
 fun main() {
 
-    val path = "src/main/input/aoc25/day11.txt"
-    val rows = File(path).readText().split(Regex("(\\r?\\n)+"))
+    val filePath = "src/main/input/aoc25/day11.txt"
+    val rows = File(filePath).readText().split(Regex("(\\r?\\n)+"))
 
 
     fun part1(rows: List<String>): Int {
@@ -27,14 +27,58 @@ fun main() {
                     paths.addAll(racks[pathEnding]!!)
                 }
             } else {
-                println("HEY $path is not part of the racks!")
+                println("HEY $pathEnding is not part of the racks!")
             }
         }
         return pathCount
     }
 
+    // does not work with full dataset
     fun part2(rows: List<String>): Int {
-        return 0
+        val racks = rows.associate { row ->
+            val rack = row.split(":")
+            rack[0] to rack[1].trim().split(Regex("\\s"))
+        }
+        println(racks)
+
+        val paths = mutableListOf(Pair("svr",Pair(false, false)))
+        var pathCount = 0
+        while (paths.isNotEmpty()){
+            println("$pathCount, $paths")
+            val pathMarker = paths.removeFirst()
+            val pathEnding = pathMarker.first
+            if(racks.keys.contains(pathEnding)){
+                if(racks[pathEnding]!!.contains("out")){
+                    if(pathMarker.second.first && pathMarker.second.second){
+                        pathCount += 1
+                    }
+
+                    for (step in racks[pathEnding]!!.filter { it != "out" }){
+                        paths.add(Pair(step, Pair(pathMarker.second.first, pathMarker.second.second)))
+                    }
+                } else {
+                    val pair = when (pathEnding) {
+                        "fft" -> {
+                            Pair(true,pathMarker.second.second)
+                        }
+                        "dac" -> {
+                            Pair(pathMarker.second.first, true)
+                        }
+                        else -> {
+                            Pair(pathMarker.second.first, pathMarker.second.second)
+                        }
+                    }
+
+                    for (step in racks[pathEnding]!!){
+                        paths.add(Pair(step, pair))
+                    }
+                }
+            } else {
+                println("HEY $pathEnding is not part of the racks!")
+            }
+        }
+
+        return pathCount
     }
 
     println(part1(rows))
